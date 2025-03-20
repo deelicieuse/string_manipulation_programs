2966e29d229050128de3b556f71658286cb1b476
deelicieuse <libatiqueangelique@gmail.com>
2025-03-21 00:08:49 +0800

    reverse the case

--- reverse_the_case_of_each_character.py
+++ reverse_the_case_of_each_character.py
@@ -1,4 +1,4 @@
 # prompt for full name in incorrect casing and reverse the case
-full_name = input("Please enter your full name in improper casing (Ex: jUan dELa crUZ): ")
+full_name = input("Please enter your full name in improper casing (Ex: jUan dELa crUZ): ").swapcase()
 
 # print result
