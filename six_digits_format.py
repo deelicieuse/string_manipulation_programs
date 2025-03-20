e6bdfdce6e985c9ea10e3665c7b81df199a20e0b
deelicieuse <libatiqueangelique@gmail.com>
2025-03-20 23:39:30 +0800

    format into six digits

--- six_digits_format.py
+++ six_digits_format.py
@@ -1,4 +1,4 @@
 # prompt fot number (0-1000) and # reformat number to six digit with leading zeros
-number = input("Please enter a number between 0 and 1000: ")
+number = input("Please enter a number between 0 and 1000: ").zfill(6)
 
 # print six digit numbr
