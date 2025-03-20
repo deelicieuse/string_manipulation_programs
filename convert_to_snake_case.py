b0e30e0f697fc8f41b501cc57536a75019b0932a
deelicieuse <libatiqueangelique@gmail.com>
2025-03-21 00:44:04 +0800

    print full name in snake case

--- convert_to_snake_case.py
+++ convert_to_snake_case.py
@@ -2,6 +2,7 @@
 full_name = input("Please enter your full name in improper casing: ")
 
 # convert to lowercase and replace spaces with "_"
-snake_case_full_name = full_name.lower(),replace(" ", "_")
+snake_case_full_name = full_name.lower().replace(" ", "_")
 
 # print snake case full name
+print(snake_case_full_name)
