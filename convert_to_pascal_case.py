fe43c606988d7a3ba04c2d5f75f1386a2f4d2c87
deelicieuse <libatiqueangelique@gmail.com>
2025-03-21 00:35:43 +0800

    enter full name

--- convert_to_pascal_case.py
+++ convert_to_pascal_case.py
@@ -1,3 +1,5 @@
 # prompt for full name in improper casing a
+full_name = input("Please enter your full name in improper casing: ")
+
 # convert to title case and remove spaces
 # print pascal case full name
