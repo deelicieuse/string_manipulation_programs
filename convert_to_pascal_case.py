f92cb725aba4c225b9e0060b583f3b43d20a7c60
deelicieuse <libatiqueangelique@gmail.com>
2025-03-21 00:39:24 +0800

    print full name in pascal case

--- convert_to_pascal_case.py
+++ convert_to_pascal_case.py
@@ -2,6 +2,7 @@
 full_name = input("Please enter your full name in improper casing: ")
 
 # convert to title case and remove spaces
-pascal_case_full_name = full_name.title().replaace(" "," ")
+pascal_case_full_name = full_name.title().replace(" ","")
 
 # print pascal case full name
+print(pascal_case_full_name)
