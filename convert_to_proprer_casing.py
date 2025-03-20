426bda82e821f9b33dc2872285a94f16fa4f2c49
deelicieuse <libatiqueangelique@gmail.com>
2025-03-21 00:04:24 +0800

    correct casing

--- convert_to_proprer_casing.py
+++ convert_to_proprer_casing.py
@@ -1,3 +1,4 @@
 # prompt for full name in incorrect casing and correct the casing
-full_name = input("Please enter your full name in improper casing (Ex: jUan dELa crUZ): ")
+full_name = input("Please enter your full name in improper casing (Ex: jUan dELa crUZ): ").title()
+
 # print corrected full name
